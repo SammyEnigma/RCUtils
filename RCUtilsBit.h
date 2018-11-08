6cd2278f4c264c731bb9489e1af8056b5cce084d
Rolando Caloca <rcaloca@gmail.com>
2018-11-08 10:33:31 -0500

    Num bits set

--- RCUtilsBit.h
+++ RCUtilsBit.h
@@ -13,3 +13,22 @@ inline T Align(T Value, T Alignment)
 	check(IsPowerOfTwo(Alignment));
 	return (Value + (Alignment - 1)) & ~(Alignment - 1);
 }
+
+
+// Naive implementation some compilers compile out to intrinsics
+template <typename T>
+inline uint32 GetNumberOfBitsSet(T N)
+{
+	uint32 Count = 0;
+	while (N > 0)
+	{
+		if (N & 1)
+		{
+			++Count;
+		}
+
+		N = N >> (T)1;
+	}
+
+	return Count;
+}
